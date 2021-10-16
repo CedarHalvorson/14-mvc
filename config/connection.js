f9346c4586e087a49fbfec9cdffb9e9d352d52a1
hat <cedarhalvorson@gmail.com>
2021-10-15 19:14:13 -0700

    checking to see if files names will update

--- config/connection.js
+++ config/connection.js
@@ -2,23 +2,6 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
-
-
-module.exports = sequelize;
-
-
-// const Sequelize = require('sequelize');
-
-// require('dotenv').config();
-
-// // create connection to our db
 // const sequelize = process.env.JAWSDB_URL
 //   ? new Sequelize(process.env.JAWSDB_URL)
 //   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
@@ -27,4 +10,21 @@ module.exports = sequelize;
 //       port: 3306
 //     });
 
+
 // module.exports = sequelize;
+
+
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
